7e2164a0ea389cd59162a2abb93e828a626f76f6
Automated <actions@users.noreply.github.com>
2022-01-04 18:25:16 +1100

    fix primary key metrics table

--- run.sh
+++ run.sh
@@ -163,7 +163,8 @@ remakeDB() {
                         json_each(d.activitydetailmetrics) m
                    )
                    select * from query order by directTimestamp desc'
-  sql-utils transform "$db" activityDetailMetrics --pk activityId --drop rowid
+  sql-utils transform "$db" activityDetailMetrics --pk activityId --pk directTimestamp
+  sql-utils transform "$db" activityDetailMetrics --drop rowid
   sql-utils drop-table "$db" details
   sql-utils create-index --if-not-exists "$db" activityDetailMetrics activityId
   sql-utils create-index --if-not-exists "$db" activityDetailMetrics activityTypeDTO_typeKey
