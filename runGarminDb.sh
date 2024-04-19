9666ba7bdef55d09248d41aa61245bd968e7855f
Automated <actions@users.noreply.github.com>
2024-04-19 09:21:16 +0200

    Latest data: Fri 19 Apr 2024 07:21:16 UTC

--- runGarminDb.sh
+++ runGarminDb.sh
@@ -6,9 +6,6 @@ set -x
 
 dockerGarminDB='garmindb'
 
-GARMIN_USERNAME='jamesmstone@hotmail.com'
-GARMIN_PASSWORD='&RkGsqNi&n9OMy1BgU2Wca$ko!5!2kEC'
-
 buildGarminDB() {
   docker build --tag "$dockerGarminDB" --pull --file garminDB.Dockerfile .
 }
